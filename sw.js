fd70fd12b40e10d332e7cfe676b8200939bdacbf
Techy Sage <142476873+techy-sage@users.noreply.github.com>
2026-08-23 17:32:43 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+
+const CACHE_NAME = 'techysage-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './app.js',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
